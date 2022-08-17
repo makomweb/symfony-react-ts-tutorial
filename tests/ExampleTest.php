e4f9fa253e919ddb15317603ceb62507eb6ade76
Martin Komischke <martin.komischke@gmail.com>
2022-08-17 13:50:40 +0200

    add example test

---
+++ tests/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function run_me(): void
+    {
+        self::assertTrue(true);
+    }
+}
