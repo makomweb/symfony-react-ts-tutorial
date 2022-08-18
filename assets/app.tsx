f7fdf169d5988d2f0584091c242539eb0891df7d
Martin Komischke <martin.komischke@gmail.com>
2022-08-18 10:25:01 +0200

    make typescript work

--- assets/app.tsx
+++ assets/app.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+interface User {
+    name: string;
+    id: number;
+}
+
+const user: User = {
+    name: "Typescript Writer",
+    id: 1
+}
+
 export default function App() {
     return (
         <div style={{ display: 'flex' }}>
-            Hello React!
+            Hello {user.name}!
         </div>
     );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -17,10 +17,10 @@ Encore
     /*
      * ENTRY CONFIG
      *
-     * Each entry will result in one JavaScript file (e.g. app.jsx)
+     * Each entry will result in one JavaScript file (e.g. app.tsx)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.jsx')
+    .addEntry('app', './assets/app.tsx')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
