674640fbcbb58863595d9d8fa848a10f819e67e7
Martin Komischke <martin.komischke@gmail.com>
2022-09-16 15:28:42 +0200

    fix score text + change color when increase in progress

--- assets/components/UserCard.tsx
+++ assets/components/UserCard.tsx
@@ -1,8 +1,22 @@
-import {Card, CardActionArea, CardContent, Typography} from "@mui/material";
+import {Card, CardActionArea, CardContent, Chip, Typography} from "@mui/material";
 import React from "react";
 
 export function UserCard(props) {
     const { user, increaseScore } = props;
+
+    const getScoreText = () => {
+        const { score } = user;
+        if (score === 0) {
+            return 'no points';
+        }
+
+        if (score === 1) {
+            return '1 point';
+        }
+
+        return `${score} points`;
+    }
+
     return (
         <Card sx={{minWidth: 275, m: 2}}>
             <CardActionArea onClick={increaseScore} disabled={user.increasing}>
@@ -10,9 +24,9 @@ export function UserCard(props) {
                     <Typography gutterBottom variant="h5" component="div">
                         {user.name}
                     </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        has {user.score} {user.score > 1 ? 'points' : 'point'}
-                    </Typography>
+                    <Chip
+                        label={getScoreText()}
+                        color={user.increasing ? 'warning': 'success'}/>
                 </CardContent>
             </CardActionArea>
         </Card>
