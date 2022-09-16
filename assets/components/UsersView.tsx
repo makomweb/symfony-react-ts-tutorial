e033053006c6daadd7375280ad802d80f09391d1
Martin Komischke <martin.komischke@gmail.com>
2022-09-16 13:16:33 +0200

    wrap the content into the same container

--- assets/components/UsersView.tsx
+++ assets/components/UsersView.tsx
@@ -6,22 +6,24 @@ import {UsersContext} from "../contexts/UsersContext";
 export default function UsersView() {
     const { users } = useContext(UsersContext);
 
-    if (users === null) {
-        return <Typography>Loading ...</Typography>;
-    }
+    const getContent = () => {
+        if (users === null) {
+            return <Typography>Loading ...</Typography>;
+        }
 
-    if (users.length === 0) {
-        return <Typography>There are no users!</Typography>;
+        if (users.length === 0) {
+            return <Typography>There are no users!</Typography>;
+        }
+
+        return users.map((user, index) =>
+            <UserCard key={index} name={user.name} age={user.age} />
+        )
     }
 
     return (
         <Container>
             <Grid container>
-                {
-                    users.map((user, index) =>
-                        <UserCard key={index} name={user.name} age={user.age} />
-                    )
-                }
+                { getContent() }
             </Grid>
         </Container>
     );
